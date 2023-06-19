eac721688bdee4ef9d76fd56d6ac8315b8103a66
Roman Vytrykush <qwertywo2000@gmail.com>
2023-06-19 19:02:50 +0300

    added Sign in test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,32 @@
 /// <reference types="cypress" />
 
+const baseUrl = 'https://react-redux.realworld.io';
+const username = 'Cytest';
+const email = '123qwe@mail.com';
+const password = 'passpass';
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
-  });
+    cy.visit(baseUrl);
+
+    cy.contains('Sign in')
+      .click();
+
+    cy.get('[type = email]')
+      .type(email);
+
+    cy.get('[type = password]')
+      .type(password);
+
+    cy.get('[type = submit]')
+      .click();
+
+    cy.get('[class = "nav-link active"]')
+      .should('contains.text', 'Your Feed');
+
+    cy.contains(username)
+      .should('contains.text', username);
+  }
+
+  );
 });
